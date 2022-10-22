38c5b2143b5e8c43b5a9b12ff31aebad2f6a0d5b
Nimrod Mallis <nimrodmallis@gmail.com>
2022-10-22 16:19:30 +0300

    Completed the math task (task 5)

--- week1/hello-turtle/math_print.py
+++ week1/hello-turtle/math_print.py
@@ -8,5 +8,31 @@
 # NOTES: N/A
 #################################################################
 
+import math
+
+def golden_ratio():
+    print((1 + math.sqrt(5)) / 2)
+
+def six_squared():
+    print(math.pow(6, 2))
+
+def hypotenuse():
+    print(math.hypot(5, 12))
+
+def pi():
+    print(math.pi)
+
+def e():
+    print(math.e)
+
+def squares_area():
+    areas = [str(math.pow(edge, 2)) for edge in range(1, 11)]
+    print(" ".join(areas))
+
 if __name__ == "__main__" :
-    pass
+    golden_ratio()
+    six_squared()
+    hypotenuse()
+    pi()
+    e()
+    squares_area()
