df2831b0c5e42aabe2aa276a97ab13d1929214a3
Nimrod Mallis <nimrodmallis@gmail.com>
2022-10-22 15:28:15 +0300

    File skeletons for the tasks

---
+++ week1/hello-turtle/hello_turtle.py
@@ -0,0 +1,12 @@
+#################################################################
+# FILE : hello_turtle.py
+# WRITER : Nimrod Mallis , mallis , 316598747
+# EXERCISE : intro2cs1 ex1 2023
+# DESCRIPTION: A simple program that...
+# STUDENTS I DISCUSSED THE EXERCISE WITH: N/A
+# WEB PAGES I USED: N/A
+# NOTES: N/A
+#################################################################
+
+if __name__ == "__main__" :
+    pass
