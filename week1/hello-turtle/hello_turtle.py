4ba6ef71eafdbda062925a3743a605ec1c02ed66
Nimrod Mallis <nimrodmallis@gmail.com>
2022-10-22 15:53:04 +0300

    Completed the 3 first parts

--- week1/hello-turtle/hello_turtle.py
+++ week1/hello-turtle/hello_turtle.py
@@ -8,5 +8,53 @@
 # NOTES: N/A
 #################################################################
 
+import turtle
+
+# Accomplishes part 1 of the task
+def draw_triangle():
+    """
+    Draws a triangle using the turtle module
+    """
+    for iter in range(3):
+        turtle.forward(45)
+        turtle.right(120)
+
+# Accomplishes part 2 of the task
+def draw_sail():
+    """
+    Draws the sail of a ship using the turtle module.
+    """
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.right(150)
+    draw_triangle()
+    turtle.right(30)
+    turtle.up()
+    turtle.forward(50)
+    turtle.down()
+    turtle.left(90)
+
+# Accomplishes part 3 of the task
+def draw_ship():
+    """
+    Draw a single ship using the turtle module.
+    """
+    # Draws the sails and board of the ship
+    for iter in range(3):
+        turtle.forward(50)
+        draw_sail()
+    turtle.forward(50)
+    # Draws the "head" of the ship
+    turtle.right(120)
+    turtle.forward(20)
+    # Draws the hull of the ship
+    turtle.right(60)
+    turtle.forward(180)
+    # Draws the "back" of the ship
+    turtle.right(60)
+    turtle.forward(20)
+    # Reset turtle orientation
+    turtle.right(120)
+
 if __name__ == "__main__" :
-    pass
+    draw_ship()
