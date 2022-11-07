19a06a216ff7430a4fe619abd83f710845926f43
Nimrod Mallis <nimrodmallis@gmail.com>
2022-11-07 09:58:19 +0200

    Implemented the first task

---
+++ week3/assignment/input_list.py
@@ -0,0 +1,34 @@
+#################################################################
+# FILE : hello_turtle.py
+# WRITER : Nimrod M.
+# EXERCISE : intro2cs1 ex1 2023
+# DESCRIPTION: Painting a fleet of ships using the turtle module
+# STUDENTS I DISCUSSED THE EXERCISE WITH: N/A
+# WEB PAGES I USED: N/A
+# NOTES: N/A
+#################################################################
+
+def input_list():
+    """
+    The function receives undefined amount of numbers from the stdin.
+    Input is terminated when the user enters an empty string.
+    All numbers given by the user are then returned in the same order in a list.
+    Including the summation of all numbers as last element of the list.
+    """
+    num_list = []
+    num_sum = 0
+
+    # Receiving the first input, this is a special case
+    user_input = input()
+    if "" == user_input:
+        return [0]
+
+    while "" != user_input:
+        user_num = float(user_input)
+        num_list.append(user_num)
+        num_sum += user_num
+        user_input = input()
+        
+    # Appending the summation as the last element
+    num_list.append(num_sum)
+    return num_list
