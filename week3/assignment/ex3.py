6d0291b4d59d83da94ad2072607aadd8b02a85a0
Nimrod Mallis <nimrodmallis@gmail.com>
2022-11-08 09:36:20 +0200

    Completed task 3

--- week3/assignment/ex3.py
+++ week3/assignment/ex3.py
@@ -47,4 +47,40 @@ def inner_product(vec1, vec2):
         product += num1 * num2
     return product
 
-print(inner_product([1], []))
+def sequence_monotonicity(sequence):
+    """
+    """
+    # The monotonicity state - Index 0 is 'Increasing', Index 1 is 'Strictly Increasing,
+    # Index 2 is 'Decreasing' and Index 3 is 'Strictly Decreasing'.
+    monotonicity = [True, True, True, True]
+    current_max = sequence[0]
+    current_min = sequence[0]
+    for num in sequence[1:]:
+        # Sequence is not increasing nor decreasing
+        if (num < current_max) and (num > current_min):
+            return [False, False, False, False]
+        # Sequence is increasing
+        elif num >= current_max:
+            if num > current_max:
+                current_max = num
+            else:
+                # Sequence is not strictly increasing, we had the same maximal value
+                monotonicity[1] = False
+            # Possibly an increasing sequence, all decreasing values are set to False
+            monotonicity[2] = False
+            monotonicity[3] = False
+        # Sequence is decreasing
+        elif num <= current_min:
+            if num < current_min:
+                current_min = num
+            else:
+                # Sequence is not strictly decreasing, we had the same minimal value
+                monotonicity[3] = False
+            # Possibly a decreasing sequence, all increasing values are set to False
+            monotonicity[0] = False
+            monotonicity[1] = False
+    return monotonicity
+        
+print(sequence_monotonicity([1,1, 2,3, 0, 4]))
+
+
