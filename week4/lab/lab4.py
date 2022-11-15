c36432c46ba0d1acc57a43b491f8e90c3bd537fe
Nimrod Mallis <nimrodmallis@gmail.com>
2022-11-15 22:09:23 +0200

    Fixing some bugs and adding docs

--- week4/lab/lab4.py
+++ week4/lab/lab4.py
@@ -1,24 +1,46 @@
+#################################################################
+# FILE : lab4.py
+# WRITER : Nimrod M.
+# EXERCISE : intro2cs1 lab4 2023
+# DESCRIPTION: Implementing the Nim Game
+# STUDENTS I DISCUSSED THE EXERCISE WITH: N/A
+# WEB PAGES I USED: N/A
+# NOTES: N/A
+#################################################################
+
 import random
 
 def init_board(rows, max_matches):
     """
+    Generating a new play board according to the requirements
+    rows (unsigned integer) - How many rows to generate
+    max_matches (unsigned integer) - How many matches to include in a row, tops (it may be less)
+    Function returns the play board generated.
     """
     return [["|" for matches in range(random.randint(1, max_matches))] for index in range(rows)]
 
 def print_board(board):
     """
+    Printing the play board in a nice and neat manner
+    Function is not returning anything
     """
     for row_index in range(len(board)):
         print("{index}: {matches}".format(index=row_index+1, matches=" ".join(board[row_index])))
 
 def is_in_range(iterable, index):
     """
+    Checking if the index is in range of the iterable, without throwing an exception :)
+    Returns true if it is, false otherwise.
     """
     return (index >= 0) and (index < len(iterable))
 
 def get_input(username, board):
     """
+    Getting the user input from the specified user and making sure the input 
+    is valid according to the received play board
+    The function returns a tuple of 2 items - The row input, and the match input.
     """
+    # First we receive the row input
     row_input = None
     while row_input is None:
         row_input = input("{username} - Select a row (from 1 to {max_row}): ".format(
@@ -27,56 +49,65 @@ def get_input(username, board):
         # Making sure the row input is indeed a decimal
         if row_input.isdecimal():
             row_input = int(row_input)-1
-        else:
+            # Making sure the row input is in range
+            if not is_in_range(board, row_input):
+                print("Invalid row {row_num} - Row is not in range!".format(row_num=row_input+1))
+                row_input = None
+        else: # Input is bad, try again!
             print("Incorrect input - \'{original_input}\' is not a decimal".format(
                 original_input=row_input))
             row_input = None
-
-        # Checking additional conditions for the row input
-        if not is_in_range(board, row_input):
-            print("Invalid row {row_num} - Row is not in range!".format(row_num=row_input+1))
-            row_input = None
-        elif 0 == len(board[row_input]):
-            print("Invalid row {row_num} - This row is empty!".format(row_num=row_input+1))
-            row_input = None
     
-    index_input = None
-    while index_input is None:
-        index_input = input("{username} - Select a index (from 1 to {max_index}): ".format(
-            username=username, max_index=len(board[row_input])))
-        if index_input.isdecimal():
-            index_input = int(index_input)-1
-        else:
-            print("Incorrect input - {original_input} is not a number".format(
-                original_input=row_input))
-            index_input = None
-
-        if not is_in_range(board[row_input], index_input):
-            print("Invalid match index {index} - Index is not in range!".format(
-                index=index_input+1))
-            index_input = None
+    # Now we get the matches input
+    match_count = None
+    while match_count is None:
+        match_count = input("{username} - Select a match count (from 1 to {max_count}): ".format(
+            username=username, max_count=len(board[row_input])))
+        
+        # Making sure the match input is indeed a decimal
+        if match_count.isdecimal():
+            match_count = int(match_count)-1
+            # Making sure the match input is in range
+            if not is_in_range(board[row_input], match_count):
+                print("Invalid match count {count} - Count is not in range!".format(
+                    count=match_count+1))
+                match_count = None
+        else: # Input is bad, try again!
+            print("Incorrect input - \'{original_input}\' is not a decimal".format(
+                original_input=match_count))
+            match_count = None
     
-    return row_input, index_input
+    return row_input, match_count
 
 def is_board_empty(board):
     """
+    Simply checking if the board is empty, if it is, the game is probably over.
+    Function returns a boolean value of whether the board is empty or not.
     """
     return 0 == len(board)
 
-def update_board(board, row, index):
+def update_board(board, row, count):
     """
+    Deleting matches from the specified row, in the amount of the specified count
+    from the given play board.
+    The function has no return value.
     """
-    for match in range(index+1):
-        del(board[row][match])
+    # Deleting matches from the 
+    for match in range(count+1):
+        del(board[row][0])
     if 0 == len(board[row]):
         del(board[row])
 
 def get_next_player(username, board):
     """
+    Starting another round for the specified username
+    Function is printing the play board, getting the input from the user and
+    updating the play board accordingly.
+    At the end, the function returns True if the player has won, or False otherwise.
     """
     print_board(board)
-    row, index = get_input(username, board)
-    update_board(board, row, index)
+    row, count = get_input(username, board)
+    update_board(board, row, count)
     # The board is empty, the player has won!
     return is_board_empty(board)
 
@@ -93,5 +124,7 @@ def run_game():
             current_player = 0
         else:
             current_player += 1
+    print("Player {username} has won!".format(username=players[current_player]))
 
+# Starting the game
 run_game()
