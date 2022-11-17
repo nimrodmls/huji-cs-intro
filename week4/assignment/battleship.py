0d389997459c3208ea332978c784bc3d3da85357
Nimrod Mallis <nimrodmallis@gmail.com>
2022-11-17 23:44:14 +0200

    Added files from Moodle

---
+++ week4/assignment/battleship.py
@@ -0,0 +1,29 @@
+import helper
+
+
+def init_board(rows, columns):
+    pass
+
+
+def cell_loc(name):
+    pass
+
+
+def valid_ship(board, size, loc):
+    pass
+
+
+def create_player_board(rows, columns, ship_sizes):
+    pass
+
+
+def fire_torpedo(board, loc):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
