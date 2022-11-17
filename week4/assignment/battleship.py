11217ab28c7509a4a80a48d686b904f5b01b8609
Nimrod Mallis <nimrodmallis@gmail.com>
2022-11-18 00:08:46 +0200

    Implemented board generator & input converter

--- week4/assignment/battleship.py
+++ week4/assignment/battleship.py
@@ -1,12 +1,36 @@
+#################################################################
+# FILE : battleship.py
+# WRITER : Nimrod M.
+# EXERCISE : intro2cs1 ex4 2023
+# DESCRIPTION: Implementing the Battleships Game
+# STUDENTS I DISCUSSED THE EXERCISE WITH: N/A
+# WEB PAGES I USED: N/A
+# NOTES: N/A
+#################################################################
+
 import helper
 
-
 def init_board(rows, columns):
-    pass
-
+    """
+    """
+    return [[helper.WATER for column in range(columns)] for row in range(rows)]
 
 def cell_loc(name):
-    pass
+    column = name[:1]
+    row = name[1:]
+
+    # Column index is not from the alphabet or it is not capital
+    if (ord('Z') < ord(column)) or (ord('A') > ord(column)):
+        return None
+    # A is the base index of columns, we decrement it from the ASCII value we received
+    column = ord(column) - ord('A')
+    
+    # Row index is not a decimal, this is wrong
+    if not row.isdecimal():
+        return None
+    row = int(row)-1 # This is safe, we decrement 1 so it would identify as list index
+
+    return row, column
 
 
 def valid_ship(board, size, loc):
@@ -22,7 +46,7 @@ def fire_torpedo(board, loc):
 
 
 def main():
-    pass
+    print(cell_loc('Z53'))
 
 
 if __name__ == "__main__":
