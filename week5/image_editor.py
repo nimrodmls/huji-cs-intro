82c3e021b6569bed53ca3d166e9574d5e880bb3c
Nimrod Mallis <nimrodmallis@gmail.com>
2022-11-22 23:55:22 +0200

    Some late-night sketches

--- week5/image_editor.py
+++ week5/image_editor.py
@@ -1,12 +1,11 @@
 #################################################################
 # FILE : image_editor.py
-# WRITER : your_name , your_login , your_id
+# WRITER : Nimrod M.
 # EXERCISE : intro2cs ex5 2022-2023
-# DESCRIPTION: A simple program that...
-# STUDENTS I DISCUSSED THE EXERCISE WITH: Bugs Bunny, b_bunny.
-#								 	      Daffy Duck, duck_daffy.
-# WEB PAGES I USED: www.looneytunes.com/lola_bunny
-# NOTES: ...
+# DESCRIPTION: TBA
+# STUDENTS I DISCUSSED THE EXERCISE WITH: N/A
+# WEB PAGES I USED: N/A
+# NOTES: N/A
 #################################################################
 
 ##############################################################################
@@ -19,50 +18,63 @@ from typing import Optional
 #                                  Functions                                 #
 ##############################################################################
 
+def get_channel(image):
+    """
+    """
+    for row 
 
 def separate_channels(image: ColoredImage) -> List[SingleChannelImage]:
-    ...
+    """
+    """
+    red_channel = []
+    blue_channel = []
+    green_channel = []
+    for row in image:
+        for pixel in row:
+            red_channel
 
+    return [[[] for pixel in row] for row in image]
+            
 
 def combine_channels(channels: List[SingleChannelImage]) -> ColoredImage:
-    ...
+    pass
 
 
 def RGB2grayscale(colored_image: ColoredImage) -> SingleChannelImage:
-    ...
+    pass
 
 
 def blur_kernel(size: int) -> Kernel:
-    ...
+    pass
 
 
 def apply_kernel(image: SingleChannelImage, kernel: Kernel) -> SingleChannelImage:
-    ...
+    pass
 
 
 def bilinear_interpolation(image: SingleChannelImage, y: float, x: float) -> int:
-    ...
+    pass
 
 
 def resize(image: SingleChannelImage, new_height: int, new_width: int) -> SingleChannelImage:
-    ...
+    pass
 
 
 def rotate_90(image: Image, direction: str) -> Image:
-    ...
+    pass
 
 
 def get_edges(image: SingleChannelImage, blur_size: int, block_size: int, c: float) -> SingleChannelImage:
-    ...
+    ...pass
 
 
 def quantize(image: SingleChannelImage, N: int) -> SingleChannelImage:
-    ...
+    pass
 
 
 def quantize_colored_image(image: ColoredImage, N: int) -> ColoredImage:
-    ...
+    pass
 
 
 if __name__ == '__main__':
-    ...
+    pass
