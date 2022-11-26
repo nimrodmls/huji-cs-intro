fd8d2f98f0620f4a475c8bdb3524e90eeb009277
Nimrod Mallis <nimrodmallis@gmail.com>
2022-11-27 00:38:11 +0200

    Kernel application now works

--- week5/image_editor.py
+++ week5/image_editor.py
@@ -13,6 +13,7 @@
 ##############################################################################
 from ex5_helper import *
 from typing import Optional
+import copy
 
 ##############################################################################
 #                                  Constants                                 #
@@ -38,6 +39,8 @@ test_rgb_image = [[[1, 2, 3], [1, 2, 3], [1, 2, 3]],
                   [[1, 2, 3], [1, 2, 3], [1, 2, 3]],
                   [[1, 2, 3], [1, 2, 3], [1, 2, 3]]]
 
+img = load_image(r"C:\users\nimrod\downloads\images.jpg")
+
 def separate_channels(image: ColoredImage) -> List[SingleChannelImage]:
     """
     """
@@ -62,10 +65,6 @@ def combine_channels(channels: List[SingleChannelImage]) -> ColoredImage:
 
     return image
 
-img = load_image(r"C:\users\nimrod\downloads\test2.jpg")
-#show_image(img)
-#show_image(combine_channels(separate_channels(img)))
-
 def calc_grayscale_sum(rgb_pixel):
     """
     """
@@ -78,85 +77,67 @@ def RGB2grayscale(colored_image: ColoredImage) -> SingleChannelImage:
     """
     return [[round(calc_grayscale_sum(pixel)) for pixel in row] for row in colored_image]
 
-#show_image(RGB2grayscale(img))
-
 def blur_kernel(size: int) -> Kernel:
     """
     """
     return [[1/(size**2) for pixel in range(size)] for row in range(size)]
-
-print(blur_kernel(3))
 
 def apply_kernel_to_matrix(matrix, kernel):
     """
     same size
     """
     matrix_sum = 0
+    kernel_center = round(len(matrix)/2)-1
+
     for row in zip(matrix, kernel):
         for pixel, kernel_cell in zip(*row):
-            matrix_sum += (pixel * kernel_cell)
+            matrix_sum += (matrix[kernel_center][kernel_center] if pixel is None else pixel) * kernel_cell
+
     return matrix_sum
 
-print(apply_kernel_to_matrix([[1,2,0], [2,2,0], [1,2,1]], [[0,0,0], [1,-1,0], [1,1,-1]]))
-
-def is_padding_needed(image, size, row_index, pixel_index):
+def get_padded_image(image, size):
     """
     """
-    horizontal_index = None
-    if 0 > (pixel_index - size):
-        horizontal_index = 0
-
-    elif (len(image[row_index])-1) < (pixel_index + size):
-        horizontal_index = pixel_index
-
-    vertical_index = None
-    if 0 > row_index-size:
-        pass
-    elif len(image)-1 == row_index:
-        pass
-
-    return horizontal_index, vertical_index
-
-def get_padded_image(image, value, size):
-    """
-    """
-    padded_image = list(image)
-    for row_pads in range(size):
-        row_pad = [value for row_len in range(len(image[0]))]
-        padded_image.insert(0, row_pad) # Insert "above"
-        padded_image.append(row_pad) # Insert "below"
+    padded_image = copy.deepcopy(image)
     
     for row_index in range(len(image)):
         for column_pads in range(size):
-            padded_image[row_index].insert(0, value) # Insert "left"
-            padded_image[row_index].append(value) # Insert "right"
+            padded_image[row_index].insert(0, None) # Insert "left"
+            padded_image[row_index].append(None) # Insert "right"
+
+    for row_pads in range(size):
+        row_pad = [None for row_len in range(len(image[0]) + size+1)]
+        padded_image.insert(0, row_pad) # Insert "above"
+        padded_image.append(row_pad) # Insert "below"
 
     return padded_image
-
-print(get_padded_image(test_rgb_image, 5, 1))
 
 def apply_kernel(image: SingleChannelImage, kernel: Kernel) -> SingleChannelImage:
     """
     """
-    padded_image = list(image)
-    for row_index in range(len(image)):
+    kernel_center = round(len(kernel)/2)
+    padded_image = get_padded_image(image, kernel_center-1)
+    
+    manipulated_image = []
+    for row_index in range(kernel_center, len(padded_image)):
+        image_row = []
 
-        padded_image
+        for column_index in range(kernel_center, len(padded_image[row_index])):
+            current_matrix = []
 
-        for pixel_index in range(len(image[row_index])):
+            for current_row in padded_image[row_index-kernel_center:row_index+kernel_center-1]:
+                current_matrix.append(current_row[column_index-kernel_center:column_index+kernel_center-1])
 
-            current_pixel = image[row_index][pixel_index]
+            image_row.append(round(apply_kernel_to_matrix(current_matrix, kernel)))
 
-            horizontal_index, vertical_index = is_padding_needed(image, round(len(kernel)/2)-1, row_index, pixel_index)
+        manipulated_image.append(image_row)
 
-            if None != horizontal_index:
-                for times in range(abs(pixel_index-len(kernel))):
-                    padded_image[row_index].insert(0, current_pixel)
+    return manipulated_image
 
-
-
-
-            cell = image[row_index][column_index]
+nimg = combine_channels([apply_kernel(separate_channels(img)[0], blur_kernel(3)),
+apply_kernel(separate_channels(img)[1], blur_kernel(3)),
+apply_kernel(separate_channels(img)[2], blur_kernel(3))])
+show_image(nimg)
 
 def bilinear_interpolation(image: SingleChannelImage, y: float, x: float) -> int:
     pass
