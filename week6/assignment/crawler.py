0f6f7c2f0659b503d847c780b1c9711c002d8ec9
Nimrod Mallis <nimrodmallis@gmail.com>
2022-12-02 22:13:52 +0200

    Added basic files for crawler, added sample index

---
+++ week6/assignment/crawler.py
@@ -0,0 +1,22 @@
+#################################################################
+# FILE : crawler.py
+# WRITER : Nimrod M.
+# EXERCISE : intro2cs1 ex6 2023
+# DESCRIPTION: Implementing the Crawler of Moogle
+# STUDENTS I DISCUSSED THE EXERCISE WITH: N/A
+# WEB PAGES I USED: N/A
+# NOTES: N/A
+#################################################################
+
+import urllib
+import requests
+import bs4
+import pickle
+
+def crawl(base_url: str, site_index):
+    """
+    :param base_url: The base URL of the website to crawl.
+    :param site_index: The website index (all relative webpage paths)
+    :return: The links table.
+    """
+    pass
---
+++ week6/assignment/moogle.py
@@ -0,0 +1,28 @@
+#################################################################
+# FILE : moogle.py
+# WRITER : Nimrod M.
+# EXERCISE : intro2cs1 ex6 2023
+# DESCRIPTION: The main program of Moogle
+# STUDENTS I DISCUSSED THE EXERCISE WITH: N/A
+# WEB PAGES I USED: N/A
+# NOTES: N/A
+#################################################################
+
+# External imports
+import argparse
+
+# Internals imports
+import crawler
+
+def _parse_parameters():
+    """
+    """
+    pass
+
+def main():
+    """
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
