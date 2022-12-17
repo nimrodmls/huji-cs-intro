f191432fb7c7b390320a138534debe34ef08b1c3
Nimrod Mallis <nimrodmallis@gmail.com>
2022-12-17 23:40:27 +0200

    Implemented picture creation with respect to constraints

--- week8/assignment/puzzle_solver.py
+++ week8/assignment/puzzle_solver.py
@@ -24,6 +24,11 @@ CONSTRAINTS_INVALID = 0
 CONSTRAINTS_VALID = 1
 CONSTRAINTS_PARTIAL = 2
 
+# Indices for the constraints tuple
+CONSTRAINT_ROW_INDEX = 0
+CONSTRAINT_COLUMN_INDEX = 1
+CONSTRAINT_SEEN_INDEX = 2
+
 def _get_cells_to_check(picture: Picture, row: int, col: int) -> Tuple[List[int], 
                                                                        List[int], 
                                                                        List[int], 
@@ -34,7 +39,7 @@ def _get_cells_to_check(picture: Picture, row: int, col: int) -> Tuple[List[int]
     # the cells are arranged FROM the cell to each direction,
     # so the cells to the right and the cells above are flipped
     row_cells_right = picture[row][:col][::-1]
-    row_cells_left = picture[row][col:]
+    row_cells_left = picture[row][col+1:]
     column_cells_above = [picture[row_index][col] for row_index in range(row)][::-1]
     column_cells_below = [picture[row_index][col] for row_index in range(row+1, len(picture))]
 
@@ -49,6 +54,8 @@ def _seen_cells(picture: Picture, row: int, col: int, count_undef=True) -> int:
     # Current cell is black, nothing is seen from here but darkness
     if picture[row][col] in invalid_cells:
         return 0
+    
+    seen_cells += 1 # Adding 1 to self, as long as it's not black cell
 
     # Going over all the relevant cells
     for cells_direction in _get_cells_to_check(picture, row, col):
@@ -75,8 +82,6 @@ def max_seen_cells(picture: Picture, row: int, col: int) -> int:
     """
     return _seen_cells(picture, row, col, count_undef=True)
 
-picture = [[-1, 0, 1, -1], [0, 1, -1, 1], [1, 0, 1, 0]] 
-
 def min_seen_cells(picture: Picture, row: int, col: int) -> int:
     """
     """
@@ -87,14 +92,18 @@ def check_constraints(picture: Picture, constraints_set: Set[Constraint]) -> int
     """
     constraint_status = CONSTRAINTS_VALID
     for constraint in constraints_set:
-        minimal_seen = min_seen_cells(picture, constraint[0], constraint[1])
-        maximal_seen = max_seen_cells(picture, constraint[0], constraint[1])
+        minimal_seen = min_seen_cells(picture, 
+                                      constraint[CONSTRAINT_ROW_INDEX], 
+                                      constraint[CONSTRAINT_COLUMN_INDEX])
+        maximal_seen = max_seen_cells(picture, 
+                                      constraint[CONSTRAINT_ROW_INDEX], 
+                                      constraint[CONSTRAINT_COLUMN_INDEX])
 
         # The minimal and maximal are different, 
         # we can't have a fully valid result at this time
         if maximal_seen != minimal_seen:
             # Checking the constraint's 'seen' value to determine the result
-            if (minimal_seen <= constraint[2]) and (maximal_seen >= constraint[2]):
+            if (minimal_seen <= constraint[CONSTRAINT_SEEN_INDEX]) and (maximal_seen >= constraint[CONSTRAINT_SEEN_INDEX]):
                 # If we happen to come across a valid constraint, 
                 # then we mark the whole result as partial 
                 # (it's enough to have 1 to determine this)
@@ -115,9 +124,60 @@ picture2 = [[0, 0, 1, 1],
 
 print(check_constraints(picture1, {(0, 3, 3), (1, 2, 5), (2, 0, 1)}))
 
-def solve_puzzle(constraints_set: Set[Constraint], n: int, m: int) -> Optional[Picture]:
-    ...
+def _set_seen_1(picture: Picture, row: int, column: int) -> Picture:
+    """
+    If we have 1 in the 'seen' field of a constraint, the solution picture
+    must have black cells surrounding the '1' cell. There's no other way.
+    """
+    if 0 != row:
+        picture[row-1][column] = BLACK_CELL
+    if len(picture)-1 != row:
+        picture[row+1][column] = BLACK_CELL
+    if 0 != column:
+        picture[row][column-1] = BLACK_CELL
+    if len(picture[row])-1 != column:
+        picture[row][column+1] = BLACK_CELL
+        
+    return picture
 
+def _create_picture(constraints_set: Set[Constraint], rows: int, columns: int) -> Picture:
+    """
+    """
+    picture = [[UNDEF_CELL for column in range(columns)] for row in range(rows)]
+    for constraint in constraints_set:
+        row_index = constraint[CONSTRAINT_ROW_INDEX]
+        column_index = constraint[CONSTRAINT_COLUMN_INDEX]
+
+        # We have two overlapping constraints - This is bad
+        if UNDEF_CELL != picture[row_index][column_index]:
+            return None
+        
+        # Choosing which color to assign to this cell
+        cell_color = WHITE_CELL
+        if 0 == constraint[CONSTRAINT_SEEN_INDEX]:
+            cell_color = BLACK_CELL
+
+        # We encountered 1 as seen, this means it's 
+        # supposed to be surrounded with black cells
+        if 1 == constraint[CONSTRAINT_SEEN_INDEX]:
+            picture = _set_seen_1(picture, row_index, column_index)
+
+        picture[row_index][column_index] = cell_color
+
+    return picture
+
+constraints_set = {(0, 0, 0), (0, 3, 3), (1, 2, 5), (2, 0, 1)}
+print(_create_picture(constraints_set, 3, 4))
+
+
+
+def solve_puzzle(constraints_set: Set[Constraint], n: int, m: int) -> Optional[Picture]:
+    """
+    """
+    picture = _create_picture(constraints_set, n, m)
+    if picture is None: # Failed to create picture with the given constraints
+        return None
+    
 
 def how_many_solutions(constraints_set: Set[Constraint], n: int, m: int) -> int:
     ...
