5d4072e28ec624d4b0c7bc5cd3a98165a9783de7
Nimrod Mallis <nimrodmallis@gmail.com>
2022-12-17 21:55:57 +0200

    First task implemented

--- week8/assignment/puzzle_solver.py
+++ week8/assignment/puzzle_solver.py
@@ -13,12 +13,61 @@ from typing import List, Tuple, Set, Optional
 
 # We define the types of a partial picture and a constraint (for type checking).
 Picture = List[List[int]]
-Constraint = Tuple[int, int, int]
+Constraint = Tuple[int, int, int] # Row, Column, Value
 
+# Cell values for the Picture
+BLACK_CELL = 0
+WHITE_CELL = 1
+UNDEF_CELL = -1 # Undefined cell
+
+def _get_cells_to_check(picture: Picture, row: int, col: int) -> Tuple[List[int], 
+                                                                       List[int], 
+                                                                       List[int], 
+                                                                       List[int]]:
+    """
+    """
+    # Arranging all the cells we need to check,
+    # the cells are arranged FROM the cell to each direction,
+    # so the cells to the right and the cells above are flipped
+    row_cells_right = picture[row][:col][::-1]
+    row_cells_left = picture[row][col:]
+    column_cells_above = [picture[row_index][col] for row_index in range(row)][::-1]
+    column_cells_below = [picture[row_index][col] for row_index in range(row+1, len(picture))]
+
+    return row_cells_left, row_cells_right, column_cells_above, column_cells_below
 
 def max_seen_cells(picture: Picture, row: int, col: int) -> int:
-    ...
+    """
+    """
+    seen_cells = 0
 
+    # Current cell is black, nothing is seen from here but darkness
+    if BLACK_CELL == picture[row][col]:
+        return 0
+
+    # Going over all the relevant cells
+    for cells_direction in _get_cells_to_check(picture, row, col):
+        current_cell = BLACK_CELL
+        current_index = 0
+
+        # If the current direction has no cells, it means we're on the edge
+        if 0 != len(cells_direction):
+            current_cell = cells_direction[current_index]
+
+        # As long as we don't meet with a black cell, continue
+        while (BLACK_CELL != current_cell) and (current_index < len(cells_direction)):
+
+            current_cell = cells_direction[current_index]
+            if BLACK_CELL != current_cell:
+                seen_cells += 1 # Incrementing the seen cells
+
+            current_index += 1 # Advancing to the next index
+
+    return seen_cells
+
+
+picture = [[-1, 0, 1, -1], [0, 1, -1, 1], [1, 0, 1, 0]] 
+print(max_seen_cells(picture, 1, 1))
 
 def min_seen_cells(picture: Picture, row: int, col: int) -> int:
     ...
