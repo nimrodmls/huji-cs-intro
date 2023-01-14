d3cde2023f8cca282550859c2e194e46ac6461da
Nimrod Mallis <nimrodmallis@gmail.com>
2023-01-14 22:22:34 +0200

    Uploaded files from Moodle

---
+++ week11/assignment/ex11_utils.py
@@ -0,0 +1,20 @@
+from typing import List, Tuple, Iterable, Optional
+
+Board = List[List[str]]
+Path = List[Tuple[int, int]]
+
+
+def is_valid_path(board: Board, path: Path, words: Iterable[str]) -> Optional[str]:
+    pass
+
+
+def find_length_n_paths(n: int, board: Board, words: Iterable[str]) -> List[Path]:
+    pass
+
+
+def find_length_n_words(n: int, board: Board, words: Iterable[str]) -> List[Path]:
+    pass
+
+
+def max_score_paths(board: Board, words: Iterable[str]) -> List[Path]:
+    pass
