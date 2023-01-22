6453057e88783434829e55a5215ca8b2b2a4b6fa
Nimrod Mallis <nimrodmallis@gmail.com>
2023-01-22 16:35:27 +0200

    Starting to set up callbacks, first working demo

--- week11/assignment/gui.py
+++ week11/assignment/gui.py
@@ -8,17 +8,30 @@
 # NOTES: N/A
 #################################################################
 
+from typing import Callable
 import tkinter as tk
 from tkinter import font, scrolledtext
 from pathlib import Path
 from boggle_board_randomizer import randomize_board
 from common import Coordinate
+from ex11_utils import Board
 
-OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Projects\huji-cs-intro\week11\assignment\build\assets\frame0")
-def relative_to_assets(path: str) -> Path:
-    return ASSETS_PATH / Path(path)
+LetterClickCallback = Callable[[Coordinate], None]
 
+class BoggleGUICallbacks(object):
+
+    def __init__(
+        self, 
+        letter_click_callback: LetterClickCallback,
+        submit_click_callback,
+        timer_callback,
+        runstate_callback) -> None:
+        """
+        """
+        self.letter_click_callback = letter_click_callback
+        self.submit_click_callback = submit_click_callback
+        self.timer_callback = timer_callback
+        self.runstate_callback = runstate_callback
 
 class BoggleGUI(object):
     """
@@ -31,16 +44,20 @@ class BoggleGUI(object):
     TEXT_COLOR = "white"
     DEFAULT_FONT = "Agency FB"
 
-    def __init__(self) -> None:
+    def __init__(self, board: Board, callbacks: BoggleGUICallbacks) -> None:
         """
         """
+        self._callbacks = callbacks
+
         self._primary_window = tk.Tk()
         self._primary_window.title("Boggle Game")
         self._primary_window.resizable(False, False)
 
         self._create_control_panel(self._primary_window)
         self._create_current_word_label(self._primary_window)
-        self._create_lower_pane(self._primary_window)
+
+        self._letter_buttons = {}
+        self._create_lower_pane(self._primary_window, board)
 
     def _loop_score(self):
         self._configure_run_state(self._runstate)
@@ -64,6 +81,10 @@ class BoggleGUI(object):
         """
         """
         self._current_word_label['text'] = current_word
+
+    def disable_letter_button(self, button_coordinate: Coordinate):
+        """
+        """
     
     def add_to_collection(self, word: str) -> None:
         """
@@ -127,12 +148,12 @@ class BoggleGUI(object):
         self._current_word_label = tk.Label(label_frame, fg="white", bg=self.BACKGROUND_COLOR_1, font=tk.font.Font(family='Agency FB', size=25, weight='bold'))
         self._current_word_label.pack(side=tk.TOP, fill=tk.BOTH)
 
-    def _create_lower_pane(self, parent_window: tk.Frame) -> None:
+    def _create_lower_pane(self, parent_window: tk.Frame, board: Board) -> None:
         """
         """
         # Initializing the lower pane to have 2 columns,
         #   one for the letter buttons table, the other for the word collection
-        lower_pane = tk.Frame(self._primary_window, bg=self.BACKGROUND_COLOR_2)
+        lower_pane = tk.Frame(parent_window, bg=self.BACKGROUND_COLOR_2)
         lower_pane.pack(side=tk.TOP, fill=tk.BOTH)
         lower_pane.columnconfigure(0, weight=4)
         lower_pane.columnconfigure(1, weight=1, minsize=200)
@@ -151,7 +172,7 @@ class BoggleGUI(object):
 
         button_table = tk.Frame(left_frame, bg=self.BACKGROUND_COLOR_2)
         button_table.grid(row=0, column=0, sticky=tk.NSEW)
-        self._create_letter_table(button_table, randomize_board())
+        self._create_letter_table(button_table, board)
 
         # Creating the submit button
         submit_button = tk.Button(
@@ -205,7 +226,7 @@ class BoggleGUI(object):
             width=25)
         self._words_collection.pack(side=tk.LEFT, fill=tk.BOTH)
 
-    def _create_letter_table(self, parent_frame: tk.Frame, board) -> None:
+    def _create_letter_table(self, parent_frame: tk.Frame, board: Board) -> None:
         """
         """
         # We rely on the fact that the board is squared
@@ -213,38 +234,37 @@ class BoggleGUI(object):
             parent_frame.rowconfigure(row_index, minsize=100)
             for column_index in range(len(board)):
                 parent_frame.columnconfigure(column_index, minsize=100)
-                button = self._create_letter_button(
+                self._create_letter_button(
                     parent_frame,
                     board[row_index][column_index],
                     row_index,
                     column_index)
-                self._letter_buttons[str(Coordinate(row_index, column_index))] = button
     
-    def _create_letter_button(self, parent, text, row, column) -> tk.Button:
+    def _create_letter_button(self, parent: tk.Frame, text: str, row: int, column: int) -> None:
         """
         """
+        button_coordinate = Coordinate(row, column)
         button = tk.Button(
             parent,
-            text=text,
+            text = text,
             bg = self.LETTER_BUTTON_COLOR,
-            fg=self.TEXT_COLOR,
-            font=tk.font.Font(family='Agency FB', size=30, weight='bold'),
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print(text))
+            fg = self.TEXT_COLOR,
+            font = font.Font(family=self.DEFAULT_FONT, size=30, weight='bold'),
+            borderwidth = 0,
+            highlightthickness = 0,
+            command = lambda: self._callbacks.letter_click_callback(button_coordinate))
         button.grid(
-            row=row, 
-            column=column, 
-            sticky=tk.NSEW, 
-            padx=10, 
-            pady=10)
+            row = row, 
+            column = column, 
+            sticky = tk.NSEW, 
+            padx = 10, 
+            pady = 10)
+            
+        self._letter_buttons[str(button_coordinate)] = (button_coordinate, button)    
         return button
 
-    def _get_asset_file(self, name: str) -> tk.PhotoImage:
-        """
-        """
-        img = tk.PhotoImage(file=self._assets_path / name)
-        return img
-
-gui = BoggleGUI()
+def demo_callback(coordinate: Coordinate):
+    print(coordinate)
+callbacks = BoggleGUICallbacks(demo_callback, None, None, None)
+gui = BoggleGUI(randomize_board(), callbacks)
 gui.start()
