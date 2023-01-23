0014791cd60dfd1e4bb0804ec68fa8e16da4125a
Nimrod Mallis <nimrodmallis@gmail.com>
2023-01-23 21:30:49 +0200

    Passing some automated tests, one still doesn't

--- week11/assignment/ex11_utils.py
+++ week11/assignment/ex11_utils.py
@@ -43,25 +43,27 @@ def _is_in_neighborhood_legacy(coordinate1: LegacyCoordinate, coordinate2: Legac
     """
     return is_in_neighborhood(Coordinate.from_legacy_coordinate(coordinate1),
                               Coordinate.from_legacy_coordinate(coordinate2))
-    
-def _assemble_word(valid_coordinates, word, last_coordinate, current_path, all_paths):
+
+def _assemble_word(valid_coordinates, word: str, last_coordinate, current_path, all_paths):
     """
     """
     if len(word) == 0:
         all_paths.append(copy.deepcopy(current_path))
         return
-
-    letter_instances = np.extract((valid_coordinates['letter'] == word[0]), valid_coordinates)
+    
+    letter_instances = np.extract((np.char.startswith(valid_coordinates['letter'], word[0])), valid_coordinates)
     if len(letter_instances) != 0:
         for instance in letter_instances:
-
-            instance_coord = instance[0]
+            if not word.startswith(letter_instances['letter'][0]):
+                continue
+            
+            instance_coord = instance['coordinate']
             if last_coordinate is not None and not _is_in_neighborhood_legacy(last_coordinate, instance_coord):
                 continue
 
             current_path.append(instance_coord)
             _assemble_word(
-                valid_coordinates[valid_coordinates != instance], word[1:], instance_coord, current_path, all_paths)
+                valid_coordinates[valid_coordinates != instance], word[len(instance['letter']):], instance_coord, current_path, all_paths)
             current_path.pop()
 
 def _find_valid_paths(path_length, board, word_dict, condition_callback = None):
@@ -78,14 +80,14 @@ def _find_valid_paths(path_length, board, word_dict, condition_callback = None):
     paths = []
     for current_comb in combinations:
         current = dict(current_comb)
-        anagram = "".join(sorted(current.values()))
+        anagram = "".join(sorted("".join(current.values())))
 
         try:
             if anagram in word_dict[len(anagram)]:
                 for word in word_dict[len(anagram)][anagram]:
                     word_paths = []
                     coordinate_map = np.array(
-                        list(current_comb), dtype=[('coordinate', tuple), ('letter', 'U1')])
+                        list(current_comb), dtype=[('coordinate', tuple), ('letter', 'U100')])
                     _assemble_word(coordinate_map, word, None, [], word_paths)
                     if ((len(word_paths) != 0) and condition_callback and condition_callback(word, word_paths)) or (not condition_callback):
                         paths += word_paths
@@ -105,6 +107,11 @@ def is_valid_path_sorted(board: Board, path: List[Coordinate], words: WordsDicti
         #   return with failure
         if (last_coordinate is not None) and (not is_in_neighborhood(last_coordinate, coordinate)):
             return None
+        
+        # Check that the coordinates are within the board boundries
+        if (coordinate.row >= len(board) or coordinate.row < 0) or \
+           (coordinate.column >= len(board[0]) or coordinate.column < 0):
+           return None
 
         # Narrowing the list of words by the letter on the current coordinate
         current_word += board[coordinate.row][coordinate.column]
@@ -128,7 +135,7 @@ def is_valid_path(board: Board, path: Path, words: Iterable[str]) -> Optional[st
     new_path = []
     for coordinate in path:
         new_path.append(Coordinate.from_legacy_coordinate(coordinate))
-    return is_valid_path_sorted(board, path, words_dict)
+    return is_valid_path_sorted(board, new_path, words_dict)
 
 def find_length_n_paths(n: int, board: Board, words: Iterable[str]) -> List[Path]:
     """
@@ -144,28 +151,35 @@ def find_length_n_words(n: int, board: Board, words: Iterable[str]) -> List[Path
     word_dict = create_words_dict(words)
     return _find_valid_paths(n, board, word_dict, lambda word, word_paths: len(word) == n)
 
-def max_score_paths_sorted(board: Board, words: WordsDictionary) -> List[Path]:
+def max_score_paths_sorted(board: Board, words_dict: WordsDictionary) -> List[Path]:
     """
     """
-    all_paths = []
+    words = {}
+
     # Iterating on the number of possible path lengths according to
     #   the board's dimensions.
     for current_path_len in range(len(board) * len(board[0])):
-        current_paths = []
 
         # A special function for selecting only 1 instance of
         #   a word instead of receiving multiple paths of the same word
         #   by the return value of the _find_valid_paths
-        def path_condition(word, word_paths, current_paths):
-            current_paths.append(word_paths[0])
+        def path_condition(word, word_paths, current_words):
+            #maximal_path = max(word_paths, key=lambda path: len(path))
+            current_words[word] = word_paths[0]
+            if word == "WENS":
+                print(current_words)
+            # if word in current_words:
+            #     if len(current_words[word]) < len(maximal_path):
+            #         current_words[word] = maximal_path
+            # else:
+            #     current_words[word] = maximal_path
             return True
 
         # We don't care about the return value as we picked only 1 instance 
         #   of the word already via path_condition
-        _find_valid_paths(current_path_len, board, words, lambda word, word_paths: path_condition(word, word_paths, current_paths))            
-        all_paths += current_paths
+        _find_valid_paths(current_path_len, board, words_dict, lambda word, word_paths: path_condition(word, word_paths, words))            
         
-    return all_paths
+    return words.values()
 
 def max_score_paths(board: Board, words: Iterable[str]) -> List[Path]:
     """
